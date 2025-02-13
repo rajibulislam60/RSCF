5f970c61c0877f4e75e938dddd0a97e4ac7f5031
rajibulislam60 <rajibulislam60707877@gmail.com>
2025-02-13 13:41:36 +0600

    tailwind and route done

--- ClientSide/src/App.jsx
+++ ClientSide/src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RootLayOut from "./RootLayOut";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<RootLayOut />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
